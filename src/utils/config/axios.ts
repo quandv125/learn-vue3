af4d4d28f54f90cd46daa61269c0aa1e9e3ba31b
quandv <quan.duong@tda.company>
2022-09-17 17:39:21 +0700

    push

--- src/utils/config/axios.ts
+++ src/utils/config/axios.ts
@@ -3,8 +3,7 @@ import axios from 'axios'
 const baseDomain = 'https://jsonplaceholder.typicode.com'
 const baseURL = `${baseDomain}` // Incase of /api/v1;
 
-// ALL default configuration here
-const token = ''
+const token = localStorage.getItem('token') || ''
 
 const apiClient = axios.create({
   baseURL,
--- src/utils/repositories/BaseRequest/index.ts
+++ src/utils/repositories/BaseRequest/index.ts
@@ -1,73 +1,74 @@
 import axiosClient from '@/utils/config/axios'
 
 export default class BaseRequest {
+  // GET METHOD
   get(url: string, params = {}) {
     return new Promise((resolve, reject) => {
       axiosClient
         .get(url, {
           params: params,
         })
-        .then(function (response) {
-          resolve(response.data)
+        .then(response => {
+          this._responseHandler(resolve, response)
         })
-        .catch(function (error) {
-          reject(error)
+        .catch(error => {
+          this._errorHandler(reject, error)
         })
     })
   }
-
+  // POST METHOD
   post(url: string, data = {}) {
     return new Promise((resolve, reject) => {
       axiosClient
         .post(url, data)
-        .then(function (response) {
-          resolve(response.data)
+        .then(response => {
+          this._responseHandler(resolve, response)
         })
-        .catch(function (error) {
-          reject(error)
+        .catch(error => {
+          this._errorHandler(reject, error)
         })
     })
   }
-
+  // PUT METHOD
   put(url: string, data = {}) {
     return new Promise((resolve, reject) => {
       axiosClient
         .put(url, data)
-        .then(function (response) {
-          resolve(response.data)
+        .then(response => {
+          this._responseHandler(resolve, response)
         })
-        .catch(function (error) {
-          reject(error)
+        .catch(error => {
+          this._errorHandler(reject, error)
         })
     })
   }
-
+  // PATCH METHOD
   patch(url: string, data = {}) {
     return new Promise((resolve, reject) => {
       axiosClient
         .patch(url, data)
-        .then(function (response) {
-          resolve(response.data)
+        .then(response => {
+          this._responseHandler(resolve, response)
         })
-        .catch(function (error) {
-          reject(error)
+        .catch(error => {
+          this._errorHandler(reject, error)
         })
     })
   }
-
+  // DEL METHOD
   del(url: string, params = {}) {
     return new Promise((resolve, reject) => {
       axiosClient
         .delete(url, { params: params })
-        .then(function (response) {
-          resolve(response.data)
+        .then(response => {
+          this._responseHandler(resolve, response)
         })
-        .catch(function (error) {
-          reject(error)
+        .catch(error => {
+          this._errorHandler(reject, error)
         })
     })
   }
-
+  // POST WITH FILE METHOD
   postWithFile(url: string, data = {}) {
     const headers = {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -76,17 +77,18 @@ export default class BaseRequest {
       axiosClient
         .post(url, data, headers)
         .then(response => {
-          resolve(response.data)
+          this._responseHandler(resolve, response)
         })
         .catch(error => {
           this._errorHandler(reject, error)
         })
     })
   }
+  // RESPONSE
   _responseHandler(resolve: any, res: any) {
     return resolve(res.body.data)
   }
-
+  // ERROR
   _errorHandler(reject: any, err: any) {
     return reject(err)
   }
