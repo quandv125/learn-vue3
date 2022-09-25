79b6dc97bbaaefc7e90cf139bb6940c798073361
quandv <quan.duong@tda.company>
2022-09-25 23:03:42 +0700

    fix

---
+++ src/types/Form.type.ts
@@ -0,0 +1,5 @@
+export enum ButtonType {
+  Submit = 'submit',
+  Button = 'button',
+  Reset = 'reset',
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,2 @@
 export * from './Login.type'
-
-export enum ButtonType {
-  Submit = 'submit',
-  Button = 'button',
-  Reset = 'reset',
-}
+export * from './Form.type'
