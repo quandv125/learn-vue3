8c5c233ff82aaabf99646a544830a818fef2fd96
quandv <quan.duong@tda.company>
2022-09-25 11:44:05 +0700

    fix

--- src/utils/validator/login.schema.ts
+++ src/utils/validator/login.schema.ts
@@ -3,3 +3,23 @@ export const loginSchema = Yup.object().shape({
   username: Yup.string().email().required('Username is required'),
   password: Yup.string().required('Password is required').min(8),
 })
+/*
+#Sample
+const schema = Yup.object().shape({
+  title: Yup.string().required('Title is required'),
+  firstName: Yup.string().required('First Name is required'),
+  lastName: Yup.string().required('Last name is required'),
+  dob: Yup.string()
+    .required('Date of Birth is required')
+    .matches(
+      /^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/,
+      'Date of Birth must be a valid date in the format YYYY-MM-DD'
+    ),
+  email: Yup.string().required('Email is required').email('Email is invalid'),
+  password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+  acceptTerms: Yup.string().required('Accept Ts & Cs is required'),
+})
+*/
