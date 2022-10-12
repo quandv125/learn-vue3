1db5310caf19ec9839039302eea1199e184c3b19
quandvtda <quan.duong@tda.company>
2022-10-12 14:32:16 +0700

    fix

---
+++ src/components/Common/FormInput/index.ts
@@ -0,0 +1,17 @@
+import TextInput from '@/components/Common/FormInput/TextInput.vue'
+import SelectOptions from '@/components/Common/FormInput/SelectOptions.vue'
+import Radio from '@/components/Common/FormInput/Radio.vue'
+import Checkbox from '@/components/Common/FormInput/Checkbox.vue'
+import Button from '@/components/Common/FormInput/Button.vue'
+import Errors from '@/components/Common/Messages/Errors.vue'
+import Modal from '@/components/Common/Modal.vue'
+
+export {
+  TextInput,
+  SelectOptions,
+  Radio,
+  Checkbox,
+  Button,
+  Errors,
+  Modal
+}
