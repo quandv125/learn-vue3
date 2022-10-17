af28d6425a77a8f2489425d9ba18123cdba278fa
quandvtda <quan.duong@tda.company>
2022-10-17 18:15:08 +0700

    add vue 3 perfect scrollbar

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,28 +1,31 @@
 <template>
   <div>
-    <table>
-      <thead>
-        <tr>
-          <td>Id</td>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Phone</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, key) in myData" :key="key">
-          <td>{{ item.id }}</td>
-          <td>{{ item.username }}</td>
-          <td>{{ item.email }}</td>
-          <td>{{ item.phone }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <perfect-scrollbar>
+      <table>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, key) in myData" :key="key">
+            <td>{{ item.id }}</td>
+            <td>{{ item.username }}</td>
+            <td>{{ item.email }}</td>
+            <td>{{ item.phone }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </perfect-scrollbar>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
 import type { TopLevel } from '@/types/fetchApi.type'
 import { usePostHelper } from '@/helpers/usePostHelper' // (1)
 const { getItinerary } = usePostHelper() // (2)
@@ -31,3 +34,10 @@ const myData = ref<TopLevel[]>([])
 
 myData.value = await getItinerary()
 </script>
+<style src="vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css"/>
+
+<style>
+.ps {
+  height: 100px;
+}
+</style>
